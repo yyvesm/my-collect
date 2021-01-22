3d810a3a0921b36056d4fcadf78d138a7e4a288e
yves <yvesmomplais@hotmail.com>
2021-01-21 20:22:17 -0500

    commit tree 4da4ce7c4b3bd2a7265353b53842fffd53a8ee08

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
+def my_collect(array)
+    i = 0
+    collect = []
+    while i < array.length
+      collect << yield(array[i])
+      i+=1
+    end
+    collect
+  end
 
+
+
+
+  my_collect(['ruby', 'javascript', 'python', 'objective-c']) do |lang|
+    lang.upcase
+  end
+
+my_collect(['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']) do |name|
+    name.split(" ").first
+end
 
